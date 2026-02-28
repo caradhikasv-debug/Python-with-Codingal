69ef1904a18a23f499636d07bbada72c3b679bb8
caradhikasv-debug <caradhikasv@gmail.com>
2026-02-28 18:46:36 +0000

    Add files via upload

---
+++ bitwise operators.py
@@ -0,0 +1,8 @@
+a=61
+b=34
+print("right shift")
+print(a>>1)
+print(b>>1)
+print("left shift")
+print(a<<1)
+print(b<<1)
---
+++ identity operator.py
@@ -0,0 +1,13 @@
+x=7
+
+if(type(x) is int):
+    print("true")
+else:
+    print("false") 
+
+o=9
+
+if(type(o)is not int):
+    print("true")
+else:
+    print("false")
