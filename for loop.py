d2be33acad3b1d6369a5700926b214c7aa06fa8e
caradhikasv-debug <caradhikasv@gmail.com>
2026-03-08 15:47:08 +0000

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,15 @@
+n=9
+for i in range(1,n+1):
+    print(i)
+
+g="Hello"
+for i in g:
+    print(i,end="@")
+
+#sum of whole number
+
+a=int(input("\n enter a number="))
+sum=0
+for i in range(1,a+1):
+    sum=sum+i
+    print(sum)
---
+++ reverseS_for loop.py
@@ -0,0 +1,5 @@
+s=input("enter your string=")
+s1=""
+for i in s:
+    s1=i+s1
+print("reversed= ",s1)
