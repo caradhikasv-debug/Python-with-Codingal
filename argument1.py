8d9910e554c02957b9371bde0c9af9dfadfc1214
caradhikasv-debug <caradhikasv@gmail.com>
2026-04-25 19:44:22 +0100

    Add files via upload

---
+++ argument1.py
@@ -0,0 +1,7 @@
+def total_calc(b,t):
+    total=b*(1+0.01*t)
+    total=round(total,2)
+    print("total bill",total)
+
+total_calc (150,20)
+total_calc (180,20)
---
+++ cube calculator.py
@@ -0,0 +1,6 @@
+def cube(n):
+    return n*n*n
+
+a=int(input("enter the number you would like to check the cube for ="))
+
+print("cube of",cube(a))
