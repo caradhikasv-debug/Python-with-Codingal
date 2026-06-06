5bec6b6b89655efe495557e9970e28040e30b84b
caradhikasv-debug <caradhikasv@gmail.com>
2026-06-06 18:59:39 +0100

    Add files via upload

--- mini calculator.py
+++ mini calculator.py
@@ -5,8 +5,12 @@ def sub(a,b):
 def mul(a,b):
     return a*b
 def div(a,b):
-    return a/b
-
+    try:
+        c= a/b
+    except ZeroDivisionError:
+        print("zerodivisionerror  check your numbers")
+    else:
+        return c
 print("SIMPLE CALCULATOR")
 print("1.ADD")
 print("2.SUBTRACT")
@@ -14,17 +18,20 @@ print("3.MULTIPLY")
 print("4.DIVIDE")
 
 c=int(input("ENTER YOUR CHOICE(1-4) = "))
-x=int(input("ENTER NUMBER 1 = "))
-y=int(input("ENTER NUMBER 2 = "))
-
-if c==1:
-    print(x,"+",y,"=",add(x,y))
-elif c==2:
-    print(x,"-",y,"=",sub(x,y))
-elif c==3:
-    print(x,"*",y,"=",mul(x,y))
-elif c==4:
-    print(x,"/",y,"=",div(x,y))
+try:
+    x=int(input("ENTER NUMBER 1 = "))
+    y=int(input("ENTER NUMBER 2 = "))
+except ValueError:
+    print("please enter a number and try again")
 else:
-    print("invalid choice")
-    
+    if c==1:
+        print(x,"+",y,"=",add(x,y))
+    elif c==2:
+        print(x,"-",y,"=",sub(x,y))
+    elif c==3:
+        print(x,"*",y,"=",mul(x,y))
+    elif c==4:
+        print(x,"/",y,"=",div(x,y))
+    else:
+        print("invalid choice")
+        
